136ee26403382fff6ebb75af65fd359d387457df
JavaKing <JavaKing@DESKTOP-32282TQ>
2019-03-13 18:24:05 +0800

    项目提交

---
+++ test01/src/test01/Test.java
@@ -0,0 +1,15 @@
+package test01;
+public class Test {
+    public void run() {
+        // TODO Auto-generated method stub
+       System.out.println("≤‚ ‘========°∑");
+       String s1=this.getClass().getResource("").getPath();
+       String s2=this.getClass().getResource("/").getPath();
+       System.out.println(s1);
+       System.out.println(s2);
+    }
+ 
+    public static void main(String[] args) {
+        new Test().run();
+    }
+}
